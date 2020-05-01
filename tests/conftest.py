89d1c739421c236f730cd663dc20c774ba05cd67
nikochiko <kaustubh299792458@gmail.com>
2020-05-01 18:53:06 +0530

    Refactor

---
+++ tests/conftest.py
@@ -0,0 +1,36 @@
+import os
+import tempfile
+
+import pytest
+
+from chess_server import main
+from chess_server.db import init_db
+
+
+@pytest.fixture
+def client():
+    db_fd, main.app.config["DATABASE"] = tempfile.mkstemp()
+    main.app.config["TESTING"] = True
+
+    with main.app.test_client() as client:
+        with main.app.app_context():
+            init_db()
+
+        yield client
+
+    os.close(db_fd)
+    os.unlink(main.app.config["DATABASE"])
+
+
+@pytest.fixture
+def context():
+    db_fd, main.app.config["DATABASE"] = tempfile.mkstemp()
+    main.app.config["TESTING"] = True
+
+    with main.app.test_request_context():
+        init_db()
+
+        yield
+
+    os.close(db_fd)
+    os.unlink(main.app.config["DATABASE"])
