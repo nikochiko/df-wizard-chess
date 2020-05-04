3352445a8ee1ef9d76b0ccb41845991be31991ea
nikochiko <kaustubh299792458@gmail.com>
2020-05-04 22:02:15 +0530

    Delete redundant part

--- chess_server/config.py
+++ chess_server/config.py
@@ -11,11 +11,3 @@ class Config:
     )  # Only the second value in the returned tuple
     IMG_DIR = os.environ.get("IMG_DIR", tempfile.mkdtemp())
     ENGINE_PATH = os.environ.get("ENGINE_PATH", "stockfish")
-
-
-def configure_app(app):
-    app.config.from_object(Config)
-    init_app(app)
-
-    if not os.path.exists(app.config["IMG_DIR"]):
-        os.mkdir(app.config["IMG_DIR"])  # Create path if does not exist
