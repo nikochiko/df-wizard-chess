738584602c09ae19f5c826d5c992c19f5c7d707e
Kaustubh Maske Patil <kaustubh299792458@gmail.com>
2020-05-20 12:29:34 +0530

    Add noqa and ignore coverage comments (#71)
    * Add noqa and ignore coverage comments
    
    * Add pragma no cover to coveragerc

--- chess_server/__init__.py
+++ chess_server/__init__.py
@@ -11,21 +11,21 @@ db = SQLAlchemy()
 def create_app(env="dev", test_config=None):
     app = Flask(__name__)
 
-    if env == "prod":
+    if env == "prod":  # pragma: no cover
         app.config.from_object("config.ProdConfig")
     elif env == "test":
         app.config.from_object("config.TestConfig")
         app.config.update(test_config or {})
-    else:
+    else:  # pragma: no cover
         app.config.from_object("config.DevConfig")
 
-    if not os.path.exists(app.config["IMG_DIR"]):
+    if not os.path.exists(app.config["IMG_DIR"]):  # pragma: no cover
         os.mkdir(app.config["IMG_DIR"])
 
     db.init_app(app)
 
     with app.app_context():
-        from chess_server import routes, models
+        from chess_server import routes, models  # noqa: F401
 
         app.register_blueprint(routes.webhook_bp)
 
